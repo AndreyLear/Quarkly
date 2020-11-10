a11894c1e5a3a66631703ca4ccfd9e3755e3df49
AndreyLear <34810848+AndreyLear@users.noreply.github.com>
2020-11-10 20:52:44 +1000

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5faa6fb8b83a2f001f2db2b2",
+				"5faa6fb8b83a2f001f2db2b4"
+			]
+		},
+		"5faa6fb8b83a2f001f2db2b2": {
+			"id": "5faa6fb8b83a2f001f2db2b2",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5faa6fb8b83a2f001f2db2b4": {
+			"id": "5faa6fb8b83a2f001f2db2b4",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
